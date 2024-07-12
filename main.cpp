fb04ebc5971ccdc7c8835635f4ff77721c8c8e86
instarlin <instarlin@gmail.com>
2024-07-13 02:31:12 +0300

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,243 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <iostream>
+#include <functional>
+#include <windows.h>
+#include <vector>
+#include <Credentials.h>
+
+using namespace std;
+// using namespace ImGui;
+using KeyCallback = std::function<void(GLFWwindow*, int, int, int, int)>;
+
+#define IMGUI_IMPL_OPENGL_LOADER_GLEW
+
+const int width = 750, height = 550;
+const char *vertexPath = VPATH;
+const char *fragmentPath = FPATH;
+int menuCase = 0;
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint fShader;
+GLuint vShader;
+GLuint vao[numVAOs];
+
+string getSystemPowerInfo() {
+    SYSTEM_POWER_STATUS sps;
+    if(!GetSystemPowerStatus(&sps)) return "Problems with detecting power info";
+
+    switch (sps.BatteryFlag) {
+    case 1:
+        return "High level";
+    case 2:
+        return "Low level";
+    case 4:
+        return "Critical level";
+    case 8:
+        return "Charging";
+    case 128:
+        return "No battery";
+    case 255:
+    default:
+        return "Unknown status";
+    }
+}
+
+void getOpenGLVersionInfo() {
+    cout << "Vendor: " << glGetString(GL_VENDOR) << endl;
+    cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "Version: " << glGetString(GL_VERSION) << endl;
+    cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    cout << "Battery status: " << getSystemPowerInfo() << endl;
+}
+
+string readShaderSource(const char *filePath, const char *type) {
+    string content;
+    ifstream fileStream(filePath);
+    if (!fileStream.is_open()) {cout << "Failed to open " << type << " shader" << endl;}
+    string line = "";
+    while (!fileStream.eof()) {
+        getline(fileStream, line);
+        content.append(line + "\n");
+    }
+    fileStream.close();
+    return content;
+}
+
+GLuint createShader(const char *glslCode, GLenum type) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, (const char**)&glslCode, NULL);
+    glCompileShader(shader);   
+    return shader;
+}
+
+void printProgramLog(int prog) {
+    int len = 0;
+    int chWrittn = 0;
+    char *log;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+    if (len > 0) {
+        log = (char *)malloc(len);
+        glGetProgramInfoLog(prog, len, &chWrittn, log);
+        cout << "Program Info Log: " << log << endl;
+        free(log);
+    }
+}
+
+GLuint createShaderProgram(int resetShader) {
+    if (resetShader == 1) glDeleteProgram(renderingProgram);
+    string vertexShaderStr = readShaderSource(vertexPath, "Vertex");
+    string fragmentShaderStr = readShaderSource(fragmentPath, "Fragment");
+    const char *vertexShader = vertexShaderStr.c_str();
+    const char *fshaderSource = fragmentShaderStr.c_str();
+
+    vShader = createShader(vertexShader, GL_VERTEX_SHADER);
+    fShader = createShader(fshaderSource, GL_FRAGMENT_SHADER);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vShader);
+    glAttachShader(shaderProgram, fShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
+    GLint result;
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &result);
+    if (result != 1) {
+        printProgramLog(shaderProgram);
+    }
+
+    return shaderProgram;
+}
+
+void Init(GLFWwindow* window) {
+    getOpenGLVersionInfo();
+    renderingProgram = createShaderProgram(0);
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+}
+
+float x = 0.0f, t = 0.0f, inc = 0.01f, tinc = 0.02f;
+
+void display(GLFWwindow* window, double currentTime) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+
+    x += inc;
+    t += tinc;
+    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
+    glProgramUniform1f(renderingProgram, offsetLoc, x);
+    GLuint timeLoc = glGetUniformLocation(renderingProgram, "time");
+    glProgramUniform1f(renderingProgram, timeLoc, t);
+    GLuint widthLoc = glGetUniformLocation(renderingProgram, "width");
+    glProgramUniform1i(renderingProgram, widthLoc, width);
+    GLuint heightLoc = glGetUniformLocation(renderingProgram, "height");
+    glProgramUniform1i(renderingProgram, heightLoc, height);
+    GLuint typeLoc = glGetUniformLocation(renderingProgram, "type");
+    glProgramUniform1i(renderingProgram, typeLoc, menuCase);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+bool is_left_ctrl_down = false;
+bool is_r_down = false;
+
+class KeyCallbackManager {
+public: 
+    void registerCallBack(KeyCallback callback) {
+        callbacks.push_back(callback);
+    }
+
+    void handleKey(GLFWwindow *window, int key, int scancode, int action, int mods) {
+        for (KeyCallback callback : callbacks) {
+            callback(window, key, scancode, action, mods);
+        }
+    }
+private:
+    vector<KeyCallback> callbacks;
+};
+
+KeyCallbackManager keyCallbackManager;
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    keyCallbackManager.handleKey(window, key, scancode, action, mods);
+}
+
+int main(void) {
+    if (!glfwInit()) exit(EXIT_FAILURE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_RESIZABLE, true);
+    GLFWwindow* window = glfwCreateWindow(width, height, "3D Scene", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) exit(EXIT_FAILURE);
+    glfwSwapInterval(1);
+    Init(window);
+
+    glfwSetKeyCallback(window, glfwKeyCallback);
+    keyCallbackManager.registerCallBack([](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        if (key == GLFW_KEY_LEFT_CONTROL) is_left_ctrl_down = action == GLFW_PRESS;
+        if (key == GLFW_KEY_R) is_r_down = action == GLFW_PRESS;
+        if (is_left_ctrl_down && is_r_down) {
+            renderingProgram = createShaderProgram(1);
+        }
+    });
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (ImGui::IsMouseClicked(ImGuiMouseButton_Right)) {
+            ImGui::OpenPopup("context_menu");
+        }
+        if (ImGui::BeginPopup("context_menu")) {
+            if (ImGui::MenuItem("Cube")) {
+                menuCase = 0;
+            }
+            if (ImGui::MenuItem("Trimmed Cubes")) { 
+                menuCase = 1;
+            }
+            if (ImGui::MenuItem("Spheres")) {
+                menuCase = 2;
+            }
+            ImGui::EndPopup();
+        }
+
+        display(window, glfwGetTime());
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
