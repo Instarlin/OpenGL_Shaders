b347563069ab2e5452fb19f3db7767a90e381d6f
instarlin <instarlin@gmail.com>
2024-07-14 13:53:56 +0300

    new file:   .gitignore 	deleted:    glsl/clouds.glsl 	renamed:    glsl/fragmentMarch750x550.glsl -> glsl/mainMarch.glsl 	modified:   glsl/noiseCloud.glsl

--- glsl/noiseCloud.glsl
+++ glsl/noiseCloud.glsl
@@ -87,10 +87,23 @@ void main(void) {
     float d = map(p);
 
     if (d < .001) {
-      float dif = clamp((d - map(p + .12*sundir))/.25, .0, 1. );
+      float dif = clamp((d - map(p + .12*sundir))/.25, .1, 1. );
       vec3 lin = vec3(.65, .65, .75)*1.1 + .8*vec3(.9, .6, .3)*dif;
       vec3 op = p;
       int counter = 0;
+      // float sd = d;
+      // float ed = 0;
+      // t += 0.001;
+      // p = ro + rd * t;
+      // d = map(p);
+      // for (int j = 0; j < 200; j++) {
+      //   t += abs(d);
+      //   ed += abs(d);
+      //   p = ro + rd * t;
+      //   d = map(p);
+      // }
+      // col += 0.08;
+      // col *= lin;
       for (int j = 1; j < 50; j++) {
         t += 0.01;
         counter += 1;
@@ -98,7 +111,7 @@ void main(void) {
         if (s > 0) break;
         p = ro + rd * t;
       };
-      col += 0.08 + 0.0005*counter;
+      col += 0.08 + 0.00005*counter;
       col *= lin;
     };
 
@@ -109,53 +122,3 @@ void main(void) {
 
   fragColor = vec4(col, 1);
 };
-
-// vec4 mainLoop(vec3 col, vec3 ro, vec3 rd, vec2 uv) {
-//   vec4 sum = vec4(0.0);
-
-//   float t = 0.;
-
-//   for (int i = 0; i < 80; i++) {
-//     float dt = max(0.05, 0.02*t);
-
-//     vec3 p = ro + rd * t;
-
-//     float d = map(p, uv);
-
-//     if (d > 0.01) {
-//       float dif = clamp((d - map(p + .3*sundir, uv))/0.25, 0., 1.);
-//       vec3 lin = vec3(0.65,0.65,0.75)*1.1 + 0.8*vec3(1.0,0.6,0.3)*dif;
-//       vec4 res = vec4( mix( vec3(1.0, 0.93, 0.84), vec3(0.25, 0.3 ,0.4), d ), d );
-//       res.xyz *= lin;
-//       res.xyz = mix(res.xyz, col, 1.0-exp2(-0.1*t));
-//       res.w = min(res.w*8.0*d,1.0);
-//       res.rgb *= res.a;
-//       sum += res*(1.0-sum.a);
-//     };
-
-//     t += d;
-
-//     if (t > 100.) break;
-//   };
-
-//   return clamp( sum, 0.0, 1.0 );
-// }
-
-// void main(void) {
-//   vec2 uv = vec2((gl_FragCoord.x*2.0-750)/550, (gl_FragCoord.y*2.0-550)/550);
-
-//   vec3 ro = vec3(0, 0, -4);
-//   vec3 rd = normalize(vec3(uv, 1));
-//   float sun = clamp( dot(sundir,rd), 0.0, 1.0 );
-//   vec3 col = vec3(0.76,0.75,0.95);
-//   // col -= 0.6*vec3(0.90,0.75,0.95)*rd.y;
-// 	// col += 0.2*vec3(1.00,0.60,0.10)*pow( sun, 8.0 );
-
-//   vec4 res = mainLoop(col, ro, rd, uv);
-
-//   col = col*(1.0-res.w) + res.xyz;
-
-//   col = smoothstep(0.15,1.1,col);
-
-//   fragColor = vec4(col, 1.0);
-// };
