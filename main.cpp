c3554d70eab45427ca00c64264c59681451728c7
instarlin <instarlin@gmail.com>
2025-01-17 22:31:18 +0300

    reassembly:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,35 +2,61 @@
 #include <GLFW/glfw3.h>
 #include <fstream>
 #include <iostream>
-#include <functional>
 #include <string>
+#include <vector>
+#include <stdexcept>
 #include <windows.h>
+
+#include <imgui/imgui.h>
 #include <imgui/backend/imgui_impl_glfw.h>
 #include <imgui/backend/imgui_impl_opengl3.h>
+
 #include <Credentials.h>
 
 using namespace std;
-using KeyCallback = std::function<void(GLFWwindow*, int, int, int, int)>;
 
-#define IMGUI_IMPL_OPENGL_LOADER_GLEW
-#define numVAOs 1
+static void GLErrorCheck(const char* msg) {
+  GLenum err = glGetError();
+  if (err != GL_NO_ERROR) {
+    std::cerr << "OpenGL error (" << msg << "): 0x" << std::hex << err << std::dec << "\n";
+  }
+}
 
-const int width = WIDTH, height = HEIGHT;
-const char* vertexPath = VPATH;
-const char* fragmentPath = FPATH;
-GLuint renderingProgram, fShader, vShader, vao[numVAOs];
-int menuCase = 0, defvalue = 0, defRotationSpeed = 12, defRenderAmount = 80;
-float x = 0.0f, t = 0.0f, inc = 0.01f, tinc = 0.02f;
-bool is_left_ctrl_down;
+constexpr int NUM_VAOS = 1;
 
-void getOpenGLVersionInfo() {
-  cout << "Vendor: " << glGetString(GL_VENDOR) << endl;
-  cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
-  cout << "Version: " << glGetString(GL_VERSION) << endl;
-  cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+struct AppData {
+  int width;
+  int height;
+
+  const char* vertexPath;
+  const char* fragmentPath;
+
+  GLuint renderingProgram;
+  GLuint vShader;
+  GLuint fShader;
+  GLuint vao[NUM_VAOS];
+
+  int   menuCase;
+  int   defValue;
+  int   defRotationSpeed;
+  int   defRenderAmount;
+
+  float x;
+  float t;
+  float inc;
+  float tinc;
+
+  bool  isLeftCtrlDown;
 };
 
-void windowTerminate(GLFWwindow* window) {
+static void printOpenGLVersionInfo() {
+  cout << "Vendor:   " << glGetString(GL_VENDOR) << endl;
+  cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+  cout << "Version:  " << glGetString(GL_VERSION) << endl;
+  cout << "Shading Language: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+}
+
+static void windowTerminate(GLFWwindow* window) {
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
@@ -38,13 +64,13 @@ void windowTerminate(GLFWwindow* window) {
   glfwDestroyWindow(window);
   glfwTerminate();
   exit(EXIT_SUCCESS);
-};
+}
 
-string readShaderSource(const std::string& filepath) {
+static std::string readShaderSource(const std::string& filepath) {
   std::ifstream file(filepath, std::ios::ate | std::ios::binary);
   if (!file.is_open()) {
-    throw std::runtime_error("Failed to open file: " + filepath);
-  };
+      throw std::runtime_error("Failed to open file: " + filepath);
+  }
 
   size_t fileSize = static_cast<size_t>(file.tellg());
   std::vector<char> buffer(fileSize);
@@ -53,203 +79,269 @@ string readShaderSource(const std::string& filepath) {
   file.read(buffer.data(), fileSize);
   file.close();
 
-  std::string fileContent(buffer.begin(), buffer.end());
-
-  return fileContent;
-};
-
-GLuint createShader(const char* glslCode, GLenum type) {
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, (const char**)&glslCode, NULL);
-    glCompileShader(shader);   
-    return shader;
+  return std::string(buffer.begin(), buffer.end());
 }
 
-void printProgramLog(int prog) {
-    int len = 0;
-    int chWrittn = 0;
-    char* log;
-    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
-    if (len > 0) {
-        log = (char*)malloc(len);
-        glGetProgramInfoLog(prog, len, &chWrittn, log);
-        cout << "Program Info Log: " << log << endl;
-        free(log);
+static GLuint createShader(const char* glslCode, GLenum type) {
+  GLuint shader = glCreateShader(type);
+  glShaderSource(shader, 1, &glslCode, nullptr);
+  glCompileShader(shader);
+  return shader;
+}
+
+static void printProgramLog(GLuint prog) {
+  int len = 0;
+  glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+  if (len > 0) {
+    std::vector<char> log(len);
+    glGetProgramInfoLog(prog, len, nullptr, log.data());
+    std::cout << "Program Info Log: " << log.data() << std::endl;
+  }
+}
+
+static GLuint createShaderProgram(AppData& data, int resetShader)
+{
+  if (resetShader == 1) {
+    glDeleteProgram(data.renderingProgram);
+  }
+
+  std::string vertexShaderStr   = readShaderSource(data.vertexPath);
+  std::string fragmentShaderStr = readShaderSource(data.fragmentPath);
+  const char* vertexShader      = vertexShaderStr.c_str();
+  const char* fshaderSource     = fragmentShaderStr.c_str();
+
+  data.vShader = createShader(vertexShader, GL_VERTEX_SHADER);
+  data.fShader = createShader(fshaderSource, GL_FRAGMENT_SHADER);
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, data.vShader);
+  glAttachShader(shaderProgram, data.fShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(data.vShader);
+  glDeleteShader(data.fShader);
+
+  GLint result;
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+  if (result != GL_TRUE) {
+    printProgramLog(shaderProgram);
+  }
+
+  return shaderProgram;
+}
+
+// GLFW callbacks – retrieve AppData via glfwGetWindowUserPointer
+static void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    AppData* data = reinterpret_cast<AppData*>(glfwGetWindowUserPointer(window));
+    if (!data) return;
+
+    // Track left ctrl
+    if (key == GLFW_KEY_LEFT_CONTROL) {
+        data->isLeftCtrlDown = (action == GLFW_PRESS);
     }
-};
 
-GLuint createShaderProgram(int resetShader) {
-    if (resetShader == 1) glDeleteProgram(renderingProgram);
-    string vertexShaderStr = readShaderSource(static_cast<std::string>(vertexPath));
-    string fragmentShaderStr = readShaderSource(static_cast<std::string>(fragmentPath));
-    const char* vertexShader = vertexShaderStr.c_str();
-    const char* fshaderSource = fragmentShaderStr.c_str();
-
-    vShader = createShader(vertexShader, GL_VERTEX_SHADER);
-    fShader = createShader(fshaderSource, GL_FRAGMENT_SHADER);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vShader);
-    glAttachShader(shaderProgram, fShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vShader);
-    glDeleteShader(fShader);
-
-    GLint result;
-    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &result);
-    if (result != 1) {
-        printProgramLog(shaderProgram);
+    if (data->isLeftCtrlDown) {
+        switch (key) {
+        case GLFW_KEY_R:
+            // Recreate shader program
+            data->renderingProgram = createShaderProgram(*data, 1);
+            break;
+        case GLFW_KEY_1:
+            data->menuCase = 0;
+            break;
+        case GLFW_KEY_2:
+            data->menuCase = 1;
+            break;
+        case GLFW_KEY_3:
+            data->menuCase = 2;
+            break;
+        case GLFW_KEY_4:
+            data->menuCase = 3;
+            break;
+        default:
+            break;
+        }
     }
+}
 
-    return shaderProgram;
-};
+static void glfwScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  AppData* data = reinterpret_cast<AppData*>(glfwGetWindowUserPointer(window));
+  if (!data) return;
 
-void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-  if (key == GLFW_KEY_LEFT_CONTROL) is_left_ctrl_down = action == GLFW_PRESS;
-  if (is_left_ctrl_down) {
-    switch (key) {
-    case GLFW_KEY_R:
-      renderingProgram = createShaderProgram(1);
-      break;
-    case GLFW_KEY_1:
-      menuCase = 0;
-      break;
-    case GLFW_KEY_2:
-      menuCase = 1;
-      break;
-    case GLFW_KEY_3:
-      menuCase = 2;
-      break;
-    case GLFW_KEY_4:
-      menuCase = 3;
-    }
-  };
-};
+  data->defValue += static_cast<int>(yoffset);
+}
 
-void glfwScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
-  defvalue += (int)yoffset;
-};
-
-void Init(GLFWwindow* window) {
+static void Init(AppData& data, GLFWwindow* window)
+{
   glfwMakeContextCurrent(window);
-  if (glewInit() != GLEW_OK) exit(EXIT_FAILURE);
-  getOpenGLVersionInfo();
 
-  renderingProgram = createShaderProgram(0);
-  glGenVertexArrays(numVAOs, vao);
-  glBindVertexArray(vao[0]);
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "GLEW init failed." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  printOpenGLVersionInfo();
+
+  data.renderingProgram = createShaderProgram(data, 0);
+
+  glGenVertexArrays(NUM_VAOS, data.vao);
+  glBindVertexArray(data.vao[0]);
+
   glfwSwapInterval(1);
   glfwSetKeyCallback(window, glfwKeyCallback);
   glfwSetScrollCallback(window, glfwScrollCallback);
 
+  // Setup ImGui
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGuiIO& io = ImGui::GetIO(); 
+  (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
   // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-  ImGui::StyleColorsDark();
 
+  ImGui::StyleColorsDark();
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL3_Init("#version 460");
-};
+}
 
-void Draw(GLFWwindow* window, double currentTime) {
-  glClearColor(0.0, 0.0, 0.0, 1.0);
+static void Draw(AppData& data, GLFWwindow* window, double currentTime)
+{
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glUseProgram(renderingProgram);
 
-  x += inc;
-  t += tinc;
-  GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-  glProgramUniform1f(renderingProgram, offsetLoc, x);
-  GLuint timeLoc = glGetUniformLocation(renderingProgram, "time");
-  glProgramUniform1f(renderingProgram, timeLoc, t);
-  GLuint widthLoc = glGetUniformLocation(renderingProgram, "width");
-  glProgramUniform1i(renderingProgram, widthLoc, width);
-  GLuint heightLoc = glGetUniformLocation(renderingProgram, "height");
-  glProgramUniform1i(renderingProgram, heightLoc, height);
-  GLuint typeLoc = glGetUniformLocation(renderingProgram, "type");
-  glProgramUniform1i(renderingProgram, typeLoc, menuCase);
-  GLuint posLoc = glGetUniformLocation(renderingProgram, "posOffset");
-  glProgramUniform1i(renderingProgram, posLoc, defvalue);
-  GLuint rotateLoc = glGetUniformLocation(renderingProgram, "rotateSpeed");
-  glProgramUniform1i(renderingProgram, rotateLoc, defRotationSpeed);
-  GLuint renderLoc = glGetUniformLocation(renderingProgram, "renderSteps");
-  glProgramUniform1i(renderingProgram, renderLoc, defRenderAmount);
+  glUseProgram(data.renderingProgram);
+
+  data.x += data.inc;
+  data.t += data.tinc;
+
+  GLuint offsetLoc = glGetUniformLocation(data.renderingProgram, "offset");
+  glProgramUniform1f(data.renderingProgram, offsetLoc, data.x);
+
+  GLuint timeLoc = glGetUniformLocation(data.renderingProgram, "time");
+  glProgramUniform1f(data.renderingProgram, timeLoc, data.t);
+
+  GLuint widthLoc = glGetUniformLocation(data.renderingProgram, "width");
+  glProgramUniform1i(data.renderingProgram, widthLoc, data.width);
+
+  GLuint heightLoc = glGetUniformLocation(data.renderingProgram, "height");
+  glProgramUniform1i(data.renderingProgram, heightLoc, data.height);
+
+  GLuint typeLoc = glGetUniformLocation(data.renderingProgram, "type");
+  glProgramUniform1i(data.renderingProgram, typeLoc, data.menuCase);
+
+  GLuint posLoc = glGetUniformLocation(data.renderingProgram, "posOffset");
+  glProgramUniform1i(data.renderingProgram, posLoc, data.defValue);
+
+  GLuint rotateLoc = glGetUniformLocation(data.renderingProgram, "rotateSpeed");
+  glProgramUniform1i(data.renderingProgram, rotateLoc, data.defRotationSpeed);
+
+  GLuint renderLoc = glGetUniformLocation(data.renderingProgram, "renderSteps");
+  glProgramUniform1i(data.renderingProgram, renderLoc, data.defRenderAmount);
 
   glDrawArrays(GL_TRIANGLES, 0, 3);
-};
+}
 
-void ImGuiDraw(GLFWwindow* window) {
+static void ImGuiDraw(AppData& data, GLFWwindow* window) {
+
   ImGui_ImplOpenGL3_NewFrame();
   ImGui_ImplGlfw_NewFrame();
   ImGui::NewFrame();
 
-  if (ImGui::IsMouseClicked(ImGuiMouseButton_Right)) ImGui::OpenPopup("context_menu");
+  if (ImGui::IsMouseClicked(ImGuiMouseButton_Right)) {
+    ImGui::OpenPopup("context_menu");
+  }
+
   if (ImGui::BeginPopup("context_menu")) {
-    if (ImGui::MenuItem("Cube")) {
-      menuCase = 0;
-    }
-    if (ImGui::MenuItem("Trimmed Cubes")) { 
-      menuCase = 1;
-    }
-    if (ImGui::MenuItem("Cubes")) {
-      menuCase = 2;
-    }
-    if (ImGui::MenuItem("Spheres")) {
-      menuCase = 3;
-    }
+    if (ImGui::MenuItem("Cube"))          { data.menuCase = 0; }
+    if (ImGui::MenuItem("Trimmed Cubes")) { data.menuCase = 1; }
+    if (ImGui::MenuItem("Cubes"))         { data.menuCase = 2; }
+    if (ImGui::MenuItem("Spheres"))       { data.menuCase = 3; }
     if (ImGui::MenuItem("Exit")) {
       exit(EXIT_SUCCESS);
     }
     ImGui::EndPopup();
-  };
+  }
 
-  switch (menuCase) {
-    case 0:
-      ImGui::SliderInt("RenderSteps", &defRenderAmount, 1, 1500);
-      ImGui::SliderInt("Distance", &defvalue, -10, 20);
-      ImGui::SliderInt("Rotation speed", &defRotationSpeed, 1, 16);
-      break;
-    case 1:
-      ImGui::SliderInt("RenderSteps", &defRenderAmount, 1, 1500);
-      ImGui::SliderInt("zSpeed", &defvalue, -10, 20);
-      break;
-    case 2:
-      ImGui::SliderInt("RenderSteps", &defRenderAmount, 1, 1500);
-      ImGui::SliderInt("Sphere zMove", &defvalue, -10, 200);
-      break;
-    case 3:
-      ImGui::SliderInt("RenderSteps", &defRenderAmount, 1, 1500);
-    default:
-      break;
-  };
+  switch (data.menuCase) {
+  case 0:
+    ImGui::SliderInt("RenderSteps",    &data.defRenderAmount, 1, 1500);
+    ImGui::SliderInt("Distance",       &data.defValue,       -10, 20);
+    ImGui::SliderInt("Rotation speed", &data.defRotationSpeed, 1, 16);
+    break;
+  case 1:
+    ImGui::SliderInt("RenderSteps", &data.defRenderAmount, 1, 1500);
+    ImGui::SliderInt("zSpeed",      &data.defValue,       -10, 20);
+    break;
+  case 2:
+    ImGui::SliderInt("RenderSteps",    &data.defRenderAmount, 1, 1500);
+    ImGui::SliderInt("Sphere zMove",   &data.defValue,       -10, 200);
+    break;
+  case 3:
+    ImGui::SliderInt("RenderSteps", &data.defRenderAmount, 1, 1500);
+    break;
+  default:
+    break;
+  }
+
   ImGui::Text("Fps: %.3f", ImGui::GetIO().Framerate);
 
+  // Render
   ImGui::Render();
   int display_w, display_h;
   glfwGetFramebufferSize(window, &display_w, &display_h);
   glViewport(0, 0, display_w, display_h);
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-};
+}
 
 int main(void) {
-  if (!glfwInit()) exit(EXIT_FAILURE);
+  if (!glfwInit()) {
+    exit(EXIT_FAILURE);
+  }
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-  glfwWindowHint(GLFW_RESIZABLE, true);
-  GLFWwindow* window = glfwCreateWindow(width, height, "3D Scene", nullptr, nullptr);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-  Init(window);
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "3D Scene", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  AppData data{};
+  data.width            = WIDTH;
+  data.height           = HEIGHT;
+  data.vertexPath       = VPATH;
+  data.fragmentPath     = FPATH;
+  data.renderingProgram = 0;
+  data.vShader          = 0;
+  data.fShader          = 0;
+  data.menuCase         = 0;
+  data.defValue         = 0;
+  data.defRotationSpeed = 12;
+  data.defRenderAmount  = 80;
+  data.x                = 0.0f;
+  data.t                = 0.0f;
+  data.inc              = 0.01f;
+  data.tinc             = 0.02f;
+  data.isLeftCtrlDown   = false;
+
+  // Make the struct accessible in GLFW callbacks
+  glfwSetWindowUserPointer(window, &data);
+
+  Init(data, window);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
-    Draw(window, glfwGetTime());
-    ImGuiDraw(window);
+    double currentTime = glfwGetTime();
+    Draw(data, window, currentTime);
+    ImGuiDraw(data, window);
 
     glfwSwapBuffers(window);
-  };
+  }
 
   windowTerminate(window);
-};
+  return 0;
+}
